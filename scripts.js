ef5a2c252100bd6a8ef13ac84463ad8ab4c66c88
Michael van Dalen <michaelvdalen191@gmail.com>
2025-03-16 10:03:05 +0200

    Added the required code to fulfill user stories

--- scripts.js
+++ scripts.js
@@ -5,5 +5,37 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  result.innerText = Math.floor(dividend / divider); // Math.floor to insure no decimal numbers
+
+  if (dividend === "" || divider === "") {
+    // If inputs are empty
+    result.innerText =
+      "Division not performed. Both values are required inputs. Try again";
+    return;
+  }
+
+  try {
+    // If divider is equal to 0
+    if (Number(divider) === 0) {
+      throw new Error(
+        "Division not performed. Invalid number provided. Try again"
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    result.innerText = error.message;
+  }
+
+  try {
+    // If either input is NaN
+    if (isNaN(dividend) || isNaN(divider)) {
+      throw new Error("Something critical went wrong. Please reload the page.");
+    }
+  } catch (error) {
+    console.log(error);
+
+    document.body.innerHTML = `<h1 style="color: red; text-align: center;">
+    Something critical went wrong. Please reload the page.</h1>`;
+  }
 });
